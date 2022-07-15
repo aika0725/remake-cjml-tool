1dac9ed93327f44711710de9dd419f0bde84941b
aika0725 <yfxx@live.com>
2022-07-15 02:09:17 +0200

    set up Appbar

---
+++ src/interfaces/Touchpoint.ts
@@ -0,0 +1,18 @@
+export interface ITouchpoint {
+  id: number
+  type: string
+  channel?: string
+  senderName: string
+  senderID: string
+  senderDescription: string
+  senderCategory?: string
+  receiverName?: string
+  receiverID?: string
+  receiverDescription?: string
+  receiverCategory?: string
+  time?: string
+}
+
+export interface ITouchpoints {
+  touchpoints: []
+}
